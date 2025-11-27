ee7e81796488651741d617f8677798adcea8fbb2
estherFrank <estherf@duck.com>
2025-11-27 17:29:46 +0000

    responsive about page with content

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,7 +4,9 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <NavBar />
-      <main className="min-h-screen px-72 py-12">{children}</main>
+      <main className="min-h-screen w-11/12 md:w-4/5 lg:w-3/5 xl:w-1/2 mx-auto py-12">
+        {children}
+      </main>
     </>
   )
 }
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,13 @@
+import aiImage from '../assets/smaller-ai-image.jpg'
+
 const About = () => {
   return (
     <>
       <h1>The Melody Maker</h1>
-      <p>This is the about page of The Melody Maker application.</p>
+      <div className="flex flex-col items-start gap-4 md:flex-row-reverse md:justify-between">
+        <img src={aiImage} className='w-60' />
+        <p>This is the about page of The Melody Maker application. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur aliquam optio nesciunt quaerat, ex aut aliquid perferendis neque natus eius iure quam obcaecati labore, quibusdam ratione. Adipisci neque aspernatur ea? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad sapiente ducimus est voluptate unde incidunt nisi et consectetur soluta voluptas, maxime atque accusantium ea veritatis quos eius pariatur similique possimus! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid assumenda repellendus voluptatem optio voluptatum enim beatae veniam perferendis eaque tenetur reprehenderit quo magnam, ipsam dicta veritatis voluptatibus consequatur ipsum nostrum. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia nobis unde cum nihil illo fugiat perferendis, quis ex blanditiis beatae, ullam consequatur iusto! Doloremque officiis impedit explicabo molestias non quibusdam.</p>
+      </div>
     </>
   )
 }
