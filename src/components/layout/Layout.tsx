691a5a4ae034a67a1115f2a22c5b9acbff9fee55
estherFrank <estherf@duck.com>
2025-12-02 20:02:09 +0000

    reduce top margin on mobile

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,7 +4,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <NavBar />
-      <main className="min-h-screen w-11/12 md:w-4/5 lg:w-3/5 xl:w-1/2 mx-auto py-12">
+      <main className="min-h-screen w-11/12 py-4 md:w-4/5 lg:w-3/5 xl:w-1/2 md:py-12 mx-auto">
         {children}
       </main>
     </>
