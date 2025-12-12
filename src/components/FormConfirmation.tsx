b5e791a61166f971011cc40922b2f7f3bdae4e26
esther-frank <estherf@duck.com>
2025-12-12 17:30:18 +0000

    add form confirmation content

--- src/components/FormConfirmation.tsx
+++ src/components/FormConfirmation.tsx
@@ -1,5 +1,18 @@
+import { NavLink } from 'react-router'
+
 const FormConfirmation = () => {
-  return <div>FormConfirmation</div>
+  return (
+    <div className="flex flex-col gap-4 items-start">
+      <h1>Thank you for contacting The Melody Maker!</h1>
+      <p>We have received your message and will get back to you shortly.</p>
+      <NavLink
+        to="/"
+        className="text-secondary bg-primary px-8 py-4 rounded-lg"
+      >
+        Return to homepage
+      </NavLink>
+    </div>
+  )
 }
 
 export default FormConfirmation
