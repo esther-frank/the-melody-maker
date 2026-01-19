49f3bd333543501967ba5c2e64176cc16b05dac2
esther-frank <estherf@duck.com>
2026-01-19 20:50:04 +0000

    fix page height

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,12 +2,12 @@ import NavBar from './NavBar'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <NavBar />
-      <main className="min-h-screen w-11/12 py-4 md:w-4/5 lg:w-3/5 xl:w-1/2 md:py-12 mx-auto">
+      <main className="flex-1 w-11/12 py-4 md:w-4/5 lg:w-3/5 xl:w-1/2 md:py-12 mx-auto">
         {children}
       </main>
-    </>
+    </div>
   )
 }
 
