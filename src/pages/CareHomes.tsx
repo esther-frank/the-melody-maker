7ffc7c105d5af5bd319c4d79572656e5d51aa6f1
esther-frank <estherf@duck.com>
2026-04-21 20:18:27 +0100

    simple list of packages

--- src/pages/CareHomes.tsx
+++ src/pages/CareHomes.tsx
@@ -1,7 +1,49 @@
-import GenreCardGrid from "../components/GenreCardGrid";
+import { NavLink } from 'react-router'
 
 const CareHomes = () => {
-  return <div><GenreCardGrid /></div>
+  const packages = [
+    'Christmas',
+    'The Beatles',
+    'Number One Hits',
+    '1950s/1960s',
+    'Love songs'
+  ]
+
+  return (
+    <div>
+      <h1 className="mb-4">The Melody Maker - melodies for your memories</h1>
+      <div className="flex flex-col gap-4">
+        <p>
+          With a comprehensive repertoire of over 150 songs from, for example,
+          Elvis, The Everly Brothers, The Beatles, Simon & Garfunkel, Abba,
+          tunes from musicals such as the Sound of Music, hymns and Christmas
+          favourites there’s always something for everyone to enjoy.
+        </p>
+        <p>
+          My performances not only engage the listener emotionally through the
+          music, but are also intellectually stimulating, being peppered with
+          anecdotes, trivia and stories about the songs and the artists.
+        </p>
+        <p>
+          My regular performances include a variety of songs from different
+          decades but, if you prefer something more specific, I also offer more
+          finely tailored packages such as:
+        </p>
+        <ul>
+          {packages.map((item) => (
+            <li className='list-disc list-inside'>{item}</li>
+          ))}
+        </ul>
+        <p>
+          To book, get in touch{' '}
+          <NavLink className="text-primary underline" to="/contact">
+            here
+          </NavLink>
+          .
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default CareHomes
