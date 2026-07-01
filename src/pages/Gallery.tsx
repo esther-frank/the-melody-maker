47351bb766ae34b095526ca659410adede02a986
esther-frank <estherf@duck.com>
2026-07-01 22:42:11 +0100

    add real video

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,16 +1,9 @@
 const Gallery = () => {
   return (
     <div className="aspect-video w-full">
-      {/* <iframe
-        className="w-full h-full"
-        src="https://www.youtube.com/embed/MtN1YnoL46Q?rel=0"
-        title="YouTube video player"
-        allow="accelerometer; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
-        loading="lazy"
-      ></iframe> */}
-      <p>Coming soon...</p>
+      <video controls className="w-full h-full">
+        <source src="https://d2nsknjzetj3dy.cloudfront.net/tmm-xs.mp4" type="video/mp4" />
+      </video>
     </div>
   )
 }
