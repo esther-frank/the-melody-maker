4ad0cba6f288e4c2549218f20804c18698ae1671
esther-frank <estherf@duck.com>
2026-07-08 21:18:06 +0100

    track active chapter

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -15,29 +15,52 @@ const chapters = [
   { title: 'Amazing Grace', timestamp: '463' }
 ]
 
-const getCurrentChapterIndex = (currentTimestamp: string) => {
-  const currentTimeAsFloat = parseFloat(currentTimestamp)
-  let activeChapterIndex = 0
-  for (let i = 1; i < chapters.length; i++) {
-    if (parseFloat(chapters[i].timestamp) > currentTimeAsFloat) {
-      return activeChapterIndex
-    }
-    console.log(`chapter ${activeChapterIndex} has already finished`)
-    activeChapterIndex++
-  }
-}
-
 const Gallery = () => {
   const videoRef = useRef<any>(null)
+  const [activeChapterIndex, setActiveChapterIndex] = useState(0)
+  const intervalRef = useRef<any>(null)
 
-  const handleClick = (timeToSeek: string) => {
+  const getCurrentChapterIndex = (currentTimestamp: string) => {
+    const currentTimeAsFloat = parseFloat(currentTimestamp)
+    let activeChapter = 0
+    for (let i = 1; i < chapters.length; i++) {
+      if (parseFloat(chapters[i].timestamp) > currentTimeAsFloat) {
+        return activeChapter
+      }
+      activeChapter++
+    }
+    return activeChapter
+  }
+
+  const runGetCurrentChapterIndex = () => {
+    clearInterval(intervalRef.current)
+    intervalRef.current = setInterval(() => {
+      setActiveChapterIndex(
+        getCurrentChapterIndex(videoRef.current.currentTime)
+      )
+    }, 1000)
+  }
+
+  const handleClick = (index: number, timeToSeek: string) => {
     if (!videoRef.current) return
     videoRef.current.currentTime = timeToSeek
+    setActiveChapterIndex(index)
   }
 
   return (
     <div className="w-full grid grid-cols-3 md:grid-cols-4 gap-2 items-start">
-      <video controls className="w-full col-span-3" ref={videoRef}>
+      <video
+        controls
+        className="w-full col-span-3"
+        ref={videoRef}
+        onPlay={runGetCurrentChapterIndex}
+        onPause={() => {
+          clearInterval(intervalRef.current)
+        }}
+        onEnded={() => {
+          clearInterval(intervalRef.current)
+        }}
+      >
         <source
           src="https://d2nsknjzetj3dy.cloudfront.net/tmm-xs.mp4"
           type="video/mp4"
@@ -45,11 +68,11 @@ const Gallery = () => {
       </video>
       <div className="flex flex-col w-full col-span-3 md:col-span-1">
         <div className="flex flex-col gap-2">
-          {chapters.map((chapter) => (
+          {chapters.map((chapter, index) => (
             <button
-              className="border p-2 cursor-pointer"
+              className={`border p-2 cursor-pointer rounded-lg ${index === activeChapterIndex ? 'bg-primary text-secondary' : 'bg-secondary text-primary'}`}
               key={`chapter-button-${chapter.title}`}
-              onClick={() => handleClick(chapter.timestamp)}
+              onClick={() => handleClick(index, chapter.timestamp)}
             >
               {chapter.title}
             </button>
